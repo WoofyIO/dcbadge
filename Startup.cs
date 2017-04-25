4081ccd60a449f31653409b19322d6bb52f4b8be
Jake Visser <javisser@microsoft.com>
2017-04-25 10:57:47 -0700

    Migation done - working on sql

--- Startup.cs
+++ Startup.cs
@@ -20,6 +20,12 @@ namespace dcbadge
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
             Configuration = builder.Build();
         }
 
