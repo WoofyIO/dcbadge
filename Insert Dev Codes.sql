60f0744ce6543de1266978c44c76f2589458c91a
Jake Visser <javisser@microsoft.com>
2017-04-26 10:52:27 -0700

    Some Dev Codes

---
+++ Insert Dev Codes.sql
@@ -0,0 +1,26 @@
+USE [dcbadge]
+GO
+
+INSERT INTO [dbo].[Codesdev]
+           ([requestcode])
+     VALUES
+           ('123456789')
+GO
+
+INSERT INTO [dbo].[Codesdev]
+           ([requestcode])
+     VALUES
+           ('987654321')
+GO
+
+INSERT INTO [dbo].[Codesdev]
+           ([requestcode])
+     VALUES
+           ('147852369')
+GO
+
+INSERT INTO [dbo].[Codesdev]
+           ([requestcode])
+     VALUES
+           ('963258741')
+GO
